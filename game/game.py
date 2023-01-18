8b0a0a286b47b556e1ef3e11ab812dac6b2dd603
hansenjiang <hansen@sidratresearch.com>
2023-01-18 16:00:00 -0500

    testing

--- game/game.py
+++ game/game.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # Piece Section
 # Colour is a bool for now: 0 for white, 1 for black
-# Position is a tuple for now: letter first, then number
+# Position is a tuple for now: letter first, then numberFF
 
 class Piece:
     def __init__(self, position:tuple, colour:bool, active: bool=True):
@@ -15,30 +15,29 @@ class Piece:
         return "implement me"
     def find_legal_moves(self):
         print("please implement me")
+        
 class Pawn(Piece):
-    def __init__(self, positiono :tuple,olouru : bool, tivev : boo,value):: int
+    def __init__(self, position :tuple, colour : bool, active: bool,value: int):
         super().__init__(position, colour, active)
         self.value = value
 
-    def find_legal_moves():self
-
+    def find_legal_moves(self):
         if self.colour == 0:
             if self.position[1] < 8:
                 if self.position[0] == 'a':
                     return [('b',self.position[1]+1)]
                 elif self.position[0] == 'b':
+                    pass
                 else:
                     legal_moves = [(self.position[0]-1, self.position[1]+1), (self.position[0]+1,self.position[1]+1)]
             else:
-
+                pass
         else:
             if self.position[1] > 1:
-                legal_moves = [(self.position[0]-1, self.position[1]+1), (self.position[0]+1,self.position[1]+1)]    def promote(self):
+                legal_moves = [(self.position[0]-1, self.position[1]+1), (self.position[0]+1,self.position[1]+1)]    
+    def promote(self):
         "implement me"
-        
-
-
-      pass
+        pass
 
 class Knight(Piece):
     def __init__(self, position : tuple, colour : bool,active : bool,value: int):
